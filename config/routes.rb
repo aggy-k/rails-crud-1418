6ebfd0960e8bbbb521654b042ff1877325c33768
Agnes Selvita <a.selvita@gmail.com>
2023-11-16 11:21:28 +0800

    Finish CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+#   get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+#   post '/restaurants', to: 'restaurants#create', as: :create_restaurants
+  
+#   # verb "path", to: 'controller_name#action_name', as: :nickname
+#   get '/restaurants', to: 'restaurants#index', as: :restaurants
+#   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  
+#   get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+#   patch '/restaurants/:id', to: 'restaurants#update', as: :update_restaurant
+
+#   delete '/restaurants/:id', to: 'restaurants#destroy', as: :destroy_restaurant
+
+  resources :restaurants, except: [:index]
+  # get '/restaurants/something', to: "restaurants#something", as: :something_restaurants
+  # resources :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    cuisine: Faker::Restaurant.type,
+    phone_number: Faker::PhoneNumber.phone_number,
+    ratings: rand(1..5)
+  )
+  restaurant.save
+  puts "Restaurant #{restaurant.name} is created."
+end
